3ead728f6b2fa72f9b85dd383321bf5cad102448
Sávio Soares Sousa <ssoaressousas@gmail.com>
2025-04-01 20:47:00 -0300

    Inicialização do repositório e configurações iniciais do projeto. Closes #1

---
+++ src/main/java/com/novacoding/lumolearn_api/LumoLearn/API/LumoLearnApiApplication.java
@@ -0,0 +1,13 @@
+package com.novacoding.lumolearn_api.LumoLearn.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumoLearnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumoLearnApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novacoding/lumolearn_api/LumoLearn/API/LumoLearnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novacoding.lumolearn_api.LumoLearn.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumoLearnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
